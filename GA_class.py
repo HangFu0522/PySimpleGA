0287ffac8b2e4ed63d2308061c5b842cd58a05d6
HangFu0522 <hangfu0522@gmail.com>
2017-08-16 01:32:19 +0800

    fix bug about obj copy

--- GA_class.py
+++ GA_class.py
@@ -2,6 +2,7 @@ import math
 import random
 import copy
 
+
 def cumsum(arg_list):
     for i in range(len(arg_list)):
         if i == 0:
@@ -14,12 +15,12 @@ def decode(bin_list):
     t = 0
     for index in range(len(bin_list)):
         t += bin_list[index] * math.pow(2, index)
-    return t * 10 / 1023
+    return t * 5 / 1023
 
 
 def my_fit(x):
-    return 10 * math.sin(5 * x) + 7 * math.cos(4 * x)
-
+    #return 10 * math.sin(5 * x) + 7 * math.cos(4 * x)
+    return 5-(x-1)*(x-5)
 
 def get_pfitvalue(fun_value):
     temp = 0
@@ -31,9 +32,7 @@ def get_pfitvalue(fun_value):
         else:
             temp = 0
         fixvalue.append(temp)
-
     total = sum(fixvalue)
-
     return [fixvalue[i] / total for i in range(number)]
 
 
@@ -57,8 +56,8 @@ class individual:
         :param arg_pMutate: 基因突变的概率
         """
         self.chromlength = arg_chromlength
-        #self.gene = [random.randint(0,1) for i in range(self.chromlength)]
-        self.gene=[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+        self.gene = [random.randint(0, 1) for i in range(self.chromlength)]
+        # self.gene=[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
         self.pMutate = arg_pMutate
 
     def mutate(self):
@@ -78,6 +77,7 @@ class Population:
         :param arg_chromlength: 基因片段的长度
         :param arg_pMutate: 基因突变的概率
         """
+
         self.number = arg_number
         self.pCross = arg_pCross
         self.chromlength = arg_chromlength
@@ -86,6 +86,9 @@ class Population:
         self.individual_list = []
         for i in range(self.number):
             self.individual_list.append(individual(self.chromlength, arg_pMutate))
+
+        self.bestgene = []
+        self.bestfunvalue = 0
 
     def cross(self):
         for index in range(self.number - 1):
@@ -119,38 +122,27 @@ class Population:
                 new_indiv_index += 1
         self.individual_list = new_individual_list[:]
 
-    def the_best(self):
-        self.bestfunvalue = self.fun_value[0]
-        self.bestone = []
-
-        for i in range(self.number):
-            if not self.bestfunvalue > self.fun_value[i]:
-                self.bestone = self.individual_list[0].gene[:]
-                self.bestfunvalue = self.fun_value[i]
-
-    def get_best_one(self):
-        return self.bestone
-
-    def get_best_value(self):
-        return self.bestfunvalue
-
+    def best(self):
+        self.fun_value = [my_fit(decode(self.individual_list[i].gene[:])) for i in range(self.number)]
+        self.bestfunvalue = max(self.fun_value)
+        self.best_index = self.fun_value.index(self.bestfunvalue)
+        self.bestgene = self.individual_list[self.best_index].gene[:]
 
 if __name__ == "__main__":
 
     pop = Population(50)
-    best_one = []
+    bestgene = []
     best_value = []
-    for i in range(1000):
+    for i in range(100):
         pop.select()
 
-        pop.the_best()
-        best_one.append(pop.get_best_one())
-        best_value.append(pop.get_best_value())
-
+        pop.best()
+        bestgene.append(pop.bestgene[:])
+        best_value.append(pop.bestfunvalue)
         pop.cross()
         pop.mutate()
 
     best_index = best_value.index(max(best_value))
-    best = best_one[best_index]
+    best = bestgene[best_index]
 
     print(decode(best))
