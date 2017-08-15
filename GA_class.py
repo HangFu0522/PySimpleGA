b7578e38430c48a169a8c6f556dd85a015b89514
HangFu0522 <hangfu0522@gmail.com>
2017-08-16 01:07:35 +0800

    ...

--- GA_class.py
+++ GA_class.py
@@ -1,134 +1,156 @@
 import math
 import random
+import copy
 
-class individual():
-    def __init__(self,arg_gene,arg_chromlength,arg_pMutate):
+def cumsum(arg_list):
+    for i in range(len(arg_list)):
+        if i == 0:
+            arg_list[i] = arg_list[0]
+        else:
+            arg_list[i] = arg_list[i] + arg_list[i - 1]
+
+
+def decode(bin_list):
+    t = 0
+    for index in range(len(bin_list)):
+        t += bin_list[index] * math.pow(2, index)
+    return t * 10 / 1023
+
+
+def my_fit(x):
+    return 10 * math.sin(5 * x) + 7 * math.cos(4 * x)
+
+
+def get_pfitvalue(fun_value):
+    temp = 0
+    fixvalue = []
+    number = len(fun_value)
+    for i in range(number):
+        if fun_value[i] > 0:
+            temp = fun_value[i]
+        else:
+            temp = 0
+        fixvalue.append(temp)
+
+    total = sum(fixvalue)
+
+    return [fixvalue[i] / total for i in range(number)]
+
+
+def exchange(indiv_1, indiv_2):
+    """
+    :type indiv_1: individual
+    :type indiv_2: individual
+    """
+    chromlength = len(indiv_1.gene)
+    exchange_point = random.randint(0, chromlength - 1)
+    gene1 = indiv_1.gene[:]
+    gene2 = indiv_2.gene[:]
+    indiv_1.gene = gene1[0:exchange_point] + gene2[exchange_point:]
+    indiv_2.gene = gene2[0:exchange_point] + gene1[exchange_point:]
+
+
+class individual:
+    def __init__(self, arg_chromlength: int, arg_pMutate: float):
         """
-        :param arg_gene: 基因编码
         :param arg_chromlength: 基因片段的长度
         :param arg_pMutate: 基因突变的概率
         """
-        self.gene=arg_gene
         self.chromlength = arg_chromlength
+        #self.gene = [random.randint(0,1) for i in range(self.chromlength)]
+        self.gene=[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
         self.pMutate = arg_pMutate
 
-    def b2d(self,bin):
-        t = 0
-        for i in range(len(bin)):
-            t += bin[i] * math.pow(2, i)
-        return t
-
-    def objfun(self,x_list):
-        x=self.b2d(x_list)*10/1023
-        return 100 - (x - 2) * (x - 7)
-
-    def fun_value(self):
-        return self.objfun(self.gene)
-
-    @property
-    def get_gene(self):
-        return self.gene
-
-    @property
-    def set_gene(self,arg_gene):
-        self.gene=arg_gene
-
     def mutate(self):
-        if random.random()<self.pMutate:
-            point=random.randint(0,self.chromlength-1)
-            if self.gene[point]==0:
-                self.gene[point] =1
+        if random.random() < self.pMutate:
+            point = random.randint(0, self.chromlength - 1)
+            if self.gene[point] == 0:
+                self.gene[point] = 1
             else:
-                self.gene[point]=0
+                self.gene[point] = 0
 
 
-
-class Population():
-    def __init__(self,arg_number,arg_pCross=0.6,arg_chromlength=10,arg_pMutate=0.001):
+class Population:
+    def __init__(self, arg_number, arg_pCross=0.6, arg_chromlength=10, arg_pMutate=0.001):
         """
         :param arg_number: 初始群体数目
         :param arg_pCross: 交换率
         :param arg_chromlength: 基因片段的长度
         :param arg_pMutate: 基因突变的概率
         """
-        self.number=arg_number
-        self.pCross=arg_pCross
+        self.number = arg_number
+        self.pCross = arg_pCross
         self.chromlength = arg_chromlength
-        self.pMutate = arg_pMutate
-        self.fun_value=[]
 
-        self.init_gene=[random.randint(0, 1) for i in range(self.chromlength)]
-        self.init_gene
-        self.individual_list=[individual(self.init_gene,self.chromlength,self.pMutate) for i in range(self.number)]
-
-
-    def __exchange__(self, indiv_1,indiv_2):
-        exchange_point=random.randint(0, self.chromlength)
-        gene1 = indiv_1.get_gene
-        gene2 = indiv_2.get_gene
-        indiv_1.gene=gene1[0:exchange_point] + gene2[exchange_point:self.chromlength]
-        indiv_2.gene=gene2[0:exchange_point] + gene1[exchange_point:self.chromlength]
+        self.fun_value = []
+        self.individual_list = []
+        for i in range(self.number):
+            self.individual_list.append(individual(self.chromlength, arg_pMutate))
 
     def cross(self):
-        for i in range(self.number):
-            if random.random()<self.pCross:
-                self.__exchange__(self.individual_list[i],self.individual_list[random.randint(0, self.number-1)])
+        for index in range(self.number - 1):
+            if random.random() < self.pCross:
+                exchange(self.individual_list[index], self.individual_list[index + 1])
 
     def mutate(self):
         for i in range(self.number):
             self.individual_list[i].mutate()
 
-    def __get_pfitvalue__(self):
-        self.fun_value=[self.individual_list[i].fun_value() for i in range(self.number)]
-        fixvalue=[self.fun_value[i] for i in range(self.number)]
-        total=sum(fixvalue)
-        return [fixvalue[i]/total for i in range(self.number)]
-
-    def __cumsum__(self,arg_fitvalue):
-        for i in range(len(arg_fitvalue)):
-            if i == 0:
-                arg_fitvalue[i] = arg_fitvalue[0]
-            else:
-                arg_fitvalue[i] = arg_fitvalue[i] + arg_fitvalue[i - 1]
+    def get_fun_value(self):
+        self.fun_value = [my_fit(decode(self.individual_list[i].gene[:])) for i in range(self.number)]
+        return self.fun_value[:]
 
     def select(self):
-        p_fitvalue=self.__get_pfitvalue__()
-        self.__cumsum__(p_fitvalue)
+        p_fitvalue = get_pfitvalue(self.get_fun_value())
+        cumsum(p_fitvalue)
 
-        p_select=[random.random() for i in range(self.number)]
+        p_select = [random.random() for i in range(self.number)]
         p_select.sort()
 
-        select_index=0
-        new_indiv_index=0
-        new_individual_list=[]
+        select_index = 0
+        new_indiv_index = 0
+        new_individual_list = []
 
-        while(select_index<self.number):
-            if p_select[select_index]<p_fitvalue[new_indiv_index]:
-                new_individual_list.append(self.individual_list[new_indiv_index])
-                select_index+=1
+        while select_index < self.number:
+            if p_select[select_index] < p_fitvalue[new_indiv_index]:
+                new_individual_list.append(copy.deepcopy(self.individual_list[new_indiv_index]))
+                select_index += 1
             else:
-                new_indiv_index+=1
-        self.individual_list=new_individual_list
+                new_indiv_index += 1
+        self.individual_list = new_individual_list[:]
 
-    @property
-    def get_best(self):
-        bestfunvalue=self.fun_value[0]
-        bestone=self.individual_list[0]
+    def the_best(self):
+        self.bestfunvalue = self.fun_value[0]
+        self.bestone = []
 
         for i in range(self.number):
-            if bestfunvalue<=self.fun_value[i]:
-                bestfunvalue=self.fun_value[i]
-                bestone=self.individual_list[i]
+            if not self.bestfunvalue > self.fun_value[i]:
+                self.bestone = self.individual_list[0].gene[:]
+                self.bestfunvalue = self.fun_value[i]
 
-        return bestfunvalue
+    def get_best_one(self):
+        return self.bestone
 
-if __name__=="__main__":
+    def get_best_value(self):
+        return self.bestfunvalue
 
-    pop=Population(500)
 
+if __name__ == "__main__":
+
+    pop = Population(50)
+    best_one = []
+    best_value = []
     for i in range(1000):
         pop.select()
+
+        pop.the_best()
+        best_one.append(pop.get_best_one())
+        best_value.append(pop.get_best_value())
+
         pop.cross()
         pop.mutate()
-    bestfunvalue=pop.get_best
-    print(bestfunvalue)
+
+    best_index = best_value.index(max(best_value))
+    best = best_one[best_index]
+
+    print(decode(best))
